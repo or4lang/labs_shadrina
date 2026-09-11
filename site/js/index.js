763903831fc246750c95a00507249136fc2b3a3f
or4lang <nadasadrina45@gmail.com>
2026-09-11 16:00:10 +0500

    add mobile version

---
+++ site/js/index.js
@@ -0,0 +1,8 @@
+// let backToTopLogoNode = document.getElementById(`logo`);
+
+// backToTopLogoNode.addEventListener(`click`, function () {
+//       window.scrollTo({
+//             top: 0,
+//             behavior: 'smooth'
+//       });
+// });
