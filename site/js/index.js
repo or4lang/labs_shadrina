ba0e85eeaa2e8325242e8aa2e6d54ab5d0867a7f
or4lang <nadasadrina45@gmail.com>
2026-09-12 23:56:46 +0500

    fix burger-menu, adaptive layout

--- site/js/index.js
+++ site/js/index.js
@@ -1,7 +1,21 @@
 let burgerNode = document.getElementById(`burger`);
 let mobileMenuNode = document.querySelector(`.mobile-menu`)
 
-burgerNode.addEventListener(`click`, function () {
-      burgerNode.classList.toggle('active');
-      mobileMenuNode.classList.toggle('active');
+window.addEventListener(`click`, function (event) {
+      if (event.target == burgerNode || burgerNode.contains(event.target)) {
+            burgerNode.classList.toggle('active');
+            mobileMenuNode.classList.toggle('active');
+      } else {
+            burgerNode.classList.remove('active');
+            mobileMenuNode.classList.remove('active');
+      }
+      // console.log(event.target);
+      // console.log(burgerNode)
 });
+
+window.addEventListener('resize', function () {
+      if (this.window.innerWidth > 768){
+            burgerNode.classList.remove('active');
+            mobileMenuNode.classList.remove('active');
+      }
+  });
